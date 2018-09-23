94fff79f5fd7143fdc9287bc581630a5b30d6829
gERBOy <johnrichard.malabanan.0916@gmail.com>
2018-09-23 20:47:02 +0800

    using host binding to set style.

--- ClientApp/src/app/directives/renderer.directive.ts
+++ ClientApp/src/app/directives/renderer.directive.ts
@@ -1,10 +1,12 @@
-import { Directive, OnInit, Renderer2, ElementRef, HostListener } from '@angular/core';
+import { Directive, OnInit, Renderer2, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
     selector: '[appRedenderDirective]'
 })
 export class RendererDirective implements OnInit {
 
+    @HostBinding('style.fontSize') fontSize:string = '12px';
+    @HostBinding('style.color') fontColor:string = 'black';
     /**
      *
      */
@@ -17,14 +19,18 @@ export class RendererDirective implements OnInit {
     }
     
     @HostListener('mouseenter') mouseEnter(eventData:Event){
-        this.renderer.setStyle(this.elementRef.nativeElement, 'font-size', '70px');
+        // this.renderer.setStyle(this.elementRef.nativeElement, 'font-size', '70px');
+        this.fontSize = '120px';
+        this.fontColor = 'red';
         this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
-        this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'white');
+        // this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'white');
     }
 
     @HostListener('mouseleave') mouseLeave(eventData:Event){
-        this.renderer.setStyle(this.elementRef.nativeElement, 'font-size', '12px');
+        this.fontSize = '12px';
+        this.fontColor = 'black';
+        // this.renderer.setStyle(this.elementRef.nativeElement, 'font-size', '12px');
         this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
-        this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'black');
+        // this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'black');
     }
 }
